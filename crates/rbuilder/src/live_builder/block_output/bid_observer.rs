bharath-123/rbuilder

commit 5c905e4c3b59e6aa843c32332322a3ec0eb585d8

feat: bid adjustments (#702)
## 📝 Summary

Add support for generating bid adjustments for relays. If the relay has
a configured fee payer address, relevant bid adjustment data will be
generated during block finalization. The adjustment data will then be
added to the block submission to the specified relay.

## ✅ I have completed the following steps:

* [x] Run `make lint`
* [x] Run `make test`
* [ ] Added tests (if applicable)

crates/rbuilder/src/live_builder/block_output/bid_observer.rs
@@ -1,10 +1,7 @@
 use alloy_primitives::U256;
 use reth_primitives::SealedBlock;
 
-use crate::{
-    building::BuiltBlockTrace, live_builder::payload_events::MevBoostSlotData,
-    mev_boost::submission::SubmitBlockRequest,
-};
+use crate::{building::BuiltBlockTrace, live_builder::payload_events::MevBoostSlotData};
 
 /// Trait that receives every bid made by us to the relays.
 pub trait BidObserver: std::fmt::Debug {
@@ -14,7 +11,6 @@ pub trait BidObserver: std::fmt::Debug {
         &self,
         slot_data: &MevBoostSlotData,
         sealed_block: &SealedBlock,
-        submit_block_request: &SubmitBlockRequest,
         built_block_trace: &BuiltBlockTrace,
         builder_name: String,
         best_bid_value: U256,
@@ -29,7 +25,6 @@ impl BidObserver for NullBidObserver {
         &self,
         _slot_data: &MevBoostSlotData,
         _sealed_block: &SealedBlock,
-        _submit_block_request: &SubmitBlockRequest,
         _built_block_trace: &BuiltBlockTrace,
         _builder_name: String,
         _best_bid_value: U256,

crates/rbuilder/src/live_builder/block_output/bid_observer_multiplexer.rs
@@ -1,9 +1,6 @@
 use reth_primitives::SealedBlock;
 
-use crate::{
-    building::BuiltBlockTrace, live_builder::payload_events::MevBoostSlotData,
-    mev_boost::submission::SubmitBlockRequest,
-};
+use crate::{building::BuiltBlockTrace, live_builder::payload_events::MevBoostSlotData};
 
 use super::bid_observer::BidObserver;
 
@@ -30,7 +27,6 @@ impl BidObserver for BidObserverMultiplexer {
         &self,
         slot_data: &MevBoostSlotData,
         sealed_block: &SealedBlock,
-        submit_block_request: &SubmitBlockRequest,
         built_block_trace: &BuiltBlockTrace,
         builder_name: String,
         best_bid_value: alloy_primitives::U256,
@@ -39,7 +35,6 @@ impl BidObserver for BidObserverMultiplexer {
             obs.block_submitted(
                 slot_data,
                 sealed_block,
-                submit_block_request,
                 built_block_trace,
                 builder_name.clone(),
                 best_bid_value,

crates/rbuilder/src/live_builder/block_output/relay_submit.rs
@@ -2,11 +2,15 @@ use crate::{
     building::builders::Block,
     live_builder::{block_output::bid_observer::BidObserver, payload_events::MevBoostSlotData},
     mev_boost::{
+        adjustment::BidAdjustmentData,
         sign_block_for_relay,
         submission::{BidMetadata, BidValueMetadata, SubmitBlockRequestWithMetadata},
-        BLSBlockSigner, RelayError, SubmitBlockErr, ValidatorSlotData,
+        BLSBlockSigner, RelayError, RelaySlotData, SubmitBlockErr, ValidatorSlotData,
     },
-    primitives::mev_boost::{MevBoostRelayBidSubmitter, MevBoostRelayID},
+    primitives::{
+        built_block::{block_to_execution_payload, SignedBuiltBlock},
+        mev_boost::{MevBoostRelayBidSubmitter, MevBoostRelayID},
+    },
     telemetry::{
         add_relay_submit_time, add_subsidy_value, inc_conn_relay_errors,
         inc_failed_block_simulations, inc_initiated_submissions, inc_other_relay_errors,
@@ -16,14 +20,14 @@ use crate::{
     utils::{duration_ms, error_storage::store_error_event},
 };
 use ahash::HashMap;
-use alloy_primitives::{utils::format_ether, U256};
+use alloy_primitives::{utils::format_ether, Address, U256};
 use mockall::automock;
 use parking_lot::Mutex;
 use reth_chainspec::ChainSpec;
 use std::sync::Arc;
 use tokio::{sync::Notify, time::Instant};
 use tokio_util::sync::CancellationToken;
-use tracing::{error, info, info_span, trace, warn, Instrument, Span};
+use tracing::{error, error_span, info, info_span, trace, warn, Instrument, Span};
 
 const SIM_ERROR_CATEGORY: &str = "submit_block_simulation";
 
@@ -233,20 +237,25 @@ async fn run_submit_to_relays_job(
         inc_initiated_submissions(optimistic_config.is_some());
         let relay_filter = get_relay_filter(&block);
 
-        let (normal_signed_submission, optimistic_signed_submission) = {
-            let normal_signed_submission = match sign_block_for_relay(
+        let execution_payload = block_to_execution_payload(
+            &config.chain_spec,
+            &slot_data.payload_attributes_event.data,
+            &block.sealed_block,
+        );
+        let (normal_signed_block, optimistic_signed_block) = {
+            let normal_signed_block = match sign_block_for_relay(
                 &config.signer,
                 &block.sealed_block,
-                &block.txs_blobs_sidecars,
-                &block.execution_requests,
-                &config.chain_spec,
                 &slot_data.payload_attributes_event.data,
                 slot_data.slot_data.pubkey,
                 block.trace.bid_value,
             ) {
-                Ok(res) => SubmitBlockRequestWithMetadata {
-                    submission: res,
-                    metadata: bid_metadata.clone(),
+                Ok((message, signature)) => SignedBuiltBlock {
+                    message,
+                    signature,
+                    execution_payload: execution_payload.clone(),
+                    blob_sidecars: block.txs_blobs_sidecars.clone(),
+                    execution_requests: block.execution_requests.clone(),
                 },
                 Err(err) => {
                     error!(parent: &submission_span, err = ?err, "Error signing block for relay");
@@ -254,21 +263,21 @@ async fn run_submit_to_relays_job(
                 }
             };
 
-            let optimistic_signed_submission = if let Some(optimistic_config) = optimistic_config {
+            let optimistic_signed_block = if let Some(optimistic_config) = optimistic_config {
                 match sign_block_for_relay(
                     &optimistic_config.signer,
                     &block.sealed_block,
-                    &block.txs_blobs_sidecars,
-                    &block.execution_requests,
-                    &config.chain_spec,
                     &slot_data.payload_attributes_event.data,
                     slot_data.slot_data.pubkey,
                     block.trace.bid_value,
                 ) {
-                    Ok(res) => Some((
-                        SubmitBlockRequestWithMetadata {
-                            submission: res,
-                            metadata: bid_metadata.clone(),
+                    Ok((message, signature)) => Some((
+                        SignedBuiltBlock {
+                            message,
+                            signature,
+                            execution_payload,
+                            blob_sidecars: block.txs_blobs_sidecars.clone(),
+                            execution_requests: block.execution_requests.clone(),
                         },
                         optimistic_config,
                     )),
@@ -281,13 +290,16 @@ async fn run_submit_to_relays_job(
                 None
             };
 
-            (normal_signed_submission, optimistic_signed_submission)
+            (normal_signed_block, optimistic_signed_block)
         };
 
         mark_submission_start_time(block.trace.orders_sealed_at);
         submit_block_to_relays(
+            &config.chain_spec,
+            &normal_signed_block,
+            &bid_metadata,
+            &block.bid_adjustments,
             &normal_relays,
-            &normal_signed_submission,
             &slot_data.relay_registrations,
             &relay_filter,
             false,
@@ -295,10 +307,13 @@ async fn run_submit_to_relays_job(
             &cancel,
         );
 
-        if let Some((optimistic_signed_submission, _)) = &optimistic_signed_submission {
+        if let Some((optimistic_signed_block, _)) = optimistic_signed_block {
             submit_block_to_relays(
+                &config.chain_spec,
+                &optimistic_signed_block,
+                &bid_metadata,
+                &block.bid_adjustments,
                 &optimistic_relays,
-                optimistic_signed_submission,
                 &slot_data.relay_registrations,
                 &relay_filter,
                 true,
@@ -308,8 +323,11 @@ async fn run_submit_to_relays_job(
         } else {
             // non-optimistic submission to optimistic relays
             submit_block_to_relays(
+                &config.chain_spec,
+                &normal_signed_block,
+                &bid_metadata,
+                &block.bid_adjustments,
                 &optimistic_relays,
-                &normal_signed_submission,
                 &slot_data.relay_registrations,
                 &relay_filter,
                 false,
@@ -323,7 +341,6 @@ async fn run_submit_to_relays_job(
             config.bid_observer.block_submitted(
                 &slot_data,
                 &block.sealed_block,
-                &normal_signed_submission.submission,
                 &block.trace,
                 builder_name,
                 bid_metadata.value.top_competitor_bid.unwrap_or_default(),
@@ -332,10 +349,14 @@ async fn run_submit_to_relays_job(
     }
 }
 
+#[allow(clippy::too_many_arguments)]
 fn submit_block_to_relays(
+    chain_spec: &ChainSpec,
+    signed_block: &SignedBuiltBlock,
+    bid_metadata: &BidMetadata,
+    bid_adjustments: &std::collections::HashMap<Address, BidAdjustmentData>,
     relays: &Vec<MevBoostRelayBidSubmitter>,
-    submission: &SubmitBlockRequestWithMetadata,
-    registrations: &HashMap<MevBoostRelayID, ValidatorSlotData>,
+    registrations: &HashMap<MevBoostRelayID, RelaySlotData>,
     relay_filter: &impl Fn(&MevBoostRelayBidSubmitter) -> bool,
     optimistic: bool,
     submission_span: &Span,
@@ -352,17 +373,33 @@ fn submit_block_to_relays(
                 }
             };
 
+            let adjustment_data = registration
+                .adjustment_fee_payer
+                .and_then(|fee_payer| bid_adjustments.get(&fee_payer).cloned());
+            let submission = match signed_block
+                .clone()
+                .into_request(chain_spec, adjustment_data)
+            {
+                Ok(submission) => SubmitBlockRequestWithMetadata {
+                    submission,
+                    metadata: bid_metadata.clone(),
+                },
+                Err(error) => {
+                    error_span!(parent: submission_span, "request_convert_error", relay = &relay.id(), ?error);
+                    continue;
+                }
+            };
+
             let span = info_span!(parent: submission_span, "relay_submit", relay = &relay.id(), optimistic);
             let relay = relay.clone();
             let cancel = cancel.clone();
-            let submission = submission.clone();
             tokio::spawn(
                 async move {
                     submit_bid_to_the_relay(
                         &relay,
                         cancel.clone(),
                         submission,
-                        registration,
+                        registration.registration,
                         optimistic,
                     )
                     .await;

Cargo.toml
@@ -99,6 +99,7 @@ ethereum_ssz = "0.9.0"
 alloy-primitives = { version = "1.3.1", default-features = false, features = ["getrandom"] }
 alloy-rlp = "0.3.10"
 alloy-chains = "0.2.5"
+alloy-trie = { version = "0.8.1", default-features = false }
 alloy-evm = { version = "0.20.1", default-features = false }
 alloy-provider = { version = "1.0.27", features = ["ipc", "pubsub", "ws"] }
 alloy-pubsub = { version = "1.0.27" }

crates/eth-sparse-mpt/Cargo.toml
@@ -16,7 +16,7 @@ serde_with = "3.9.0"
 rustc-hash = "2.0.0"
 rayon = "1.10.0"
 smallvec = "1.13.2"
-alloy-trie = { version = "0.8.1", default-features = false }
+alloy-trie.workspace = true
 nybbles = { version = "0.3.3" }
 
 tracing.workspace = true

crates/eth-sparse-mpt/src/lib.rs
@@ -6,12 +6,12 @@
 #![allow(clippy::large_enum_variant)]
 #![allow(clippy::type_complexity)]
 
-use std::sync::Arc;
-
-use alloy_primitives::{Address, B256};
+use crate::utils::{HashMap, HashSet};
+use alloy_primitives::{Address, Bytes, B256};
 use reth_provider::{
     providers::ConsistentDbView, BlockReader, DatabaseProviderFactory, ExecutionOutcome,
 };
+use std::sync::Arc;
 
 #[cfg(any(test, feature = "benchmark-utils"))]
 pub mod test_utils;
@@ -138,6 +138,48 @@ impl SparseTrieError {
     }
 }
 
+pub fn calculate_account_proofs_with_sparse_trie<Provider>(
+    consistent_db_view: ConsistentDbView<Provider>,
+    outcome: &ExecutionOutcome,
+    proof_targets: &HashSet<Address>,
+    shared_cache: &SparseTrieSharedCache,
+    local_cache: &mut SparseTrieLocalCache,
+    thread_pool: &Option<RootHashThreadPool>,
+    version: ETHSpareMPTVersion,
+) -> (
+    Result<HashMap<Address, Vec<Bytes>>, SparseTrieError>,
+    SparseTrieMetrics,
+)
+where
+    Provider: DatabaseProviderFactory<Provider: BlockReader> + Send + Sync,
+{
+    let calculate = || match version {
+        ETHSpareMPTVersion::V1 => (
+            Err(SparseTrieError::Other(eyre::eyre!(
+                "proof generation not supported in v1"
+            ))),
+            Default::default(),
+        ),
+        ETHSpareMPTVersion::V2 => {
+            let result = local_cache.calc.calculate_root_hash_with_sparse_trie(
+                consistent_db_view,
+                shared_cache.cache_v2.clone(),
+                outcome,
+                proof_targets,
+            );
+            match result {
+                Ok((_, proofs, metrics)) => (Ok(proofs), metrics),
+                Err(err) => (Err(err), Default::default()),
+            }
+        }
+    };
+    if let Some(thread_pool) = thread_pool {
+        thread_pool.rayon_pool.install(calculate)
+    } else {
+        calculate()
+    }
+}
+
 pub fn calculate_root_hash_with_sparse_trie<Provider>(
     consistent_db_view: ConsistentDbView<Provider>,
     outcome: &ExecutionOutcome,
@@ -197,9 +239,10 @@ where
                 consistent_db_view,
                 shared_cache.cache_v2.clone(),
                 outcome,
+                &Default::default(),
             );
             match result {
-                Ok((res, metrics)) => (Ok(res), metrics),
+                Ok((res, _, metrics)) => (Ok(res), metrics),
                 Err(err) => (Err(err), Default::default()),
             }
         }

crates/eth-sparse-mpt/src/v2/mod.rs
@@ -1,24 +1,22 @@
-use rayon::prelude::*;
-use std::sync::Arc;
-use std::time::Instant;
-
-use alloy_primitives::{keccak256, Address, B256, U256};
+use alloy_primitives::{keccak256, Address, Bytes, B256, U256};
 use dashmap::DashMap;
 use fetch::MissingNodesFetcher;
 use nybbles::Nibbles;
 use parking_lot::{Mutex, RwLock};
+use rayon::prelude::*;
 use reth_provider::{
     providers::ConsistentDbView, BlockReader, DatabaseProviderFactory, ExecutionOutcome,
 };
 use reth_trie::TrieAccount;
 use revm::state::AccountInfo;
 use rustc_hash::FxBuildHasher;
-use std::ops::Range;
-use trie::proof_store::ProofStore;
-use trie::{DeletionError, InsertValue, NodeNotFound, Trie};
+use std::{ops::Range, sync::Arc, time::Instant};
+use trie::{proof_store::ProofStore, DeletionError, InsertValue, NodeNotFound, Trie};
 
-use crate::utils::HashMap;
-use crate::{ChangedAccountData, SparseTrieError, SparseTrieMetrics};
+use crate::{
+    utils::{HashMap, HashSet},
+    ChangedAccountData, SparseTrieError, SparseTrieMetrics,
+};
 
 pub mod fetch;
 pub mod trie;
@@ -277,7 +275,11 @@ impl RootHashCalculator {
         }
     }
 
-    fn prepare_changes_for_storage_trie(&mut self, outcome: &ExecutionOutcome) -> eyre::Result<()> {
+    fn prepare_changes_for_storage_trie(
+        &mut self,
+        outcome: &ExecutionOutcome,
+        proof_targets: &HashSet<Address>,
+    ) -> eyre::Result<()> {
         self.changed_account.write().clear();
 
         outcome
@@ -354,6 +356,20 @@ impl RootHashCalculator {
                 storage_calc.applied_storage_ops_previous_iteration =
                     applied_storage_ops_previous_iteration;
             });
+
+        for proof_target in proof_targets {
+            if outcome
+                .bundle
+                .state
+                .get(proof_target)
+                .is_none_or(|acc| !acc.status.is_not_modified())
+            {
+                self.changed_account
+                    .write()
+                    .push((*proof_target, StorageTrieStatus::Hashed));
+            }
+        }
+
         Ok(())
     }
 
@@ -564,23 +580,23 @@ impl RootHashCalculator {
         let fetcher = Arc::new(Mutex::new(MissingNodesFetcher::default()));
 
         self.changed_account.read().par_iter().for_each(|(address, status)| {
-	    if !status.needs_processing() {
-		return;
-	    }
+            if !status.needs_processing() {
+                return;
+            }
             let storage_calc = self.get_account_storage(address);
-	    let mut storage_calc = storage_calc.lock();
-	    let storage_calc = &mut *storage_calc;
-	    storage_calc.missing_nodes_requested.clear();
-	    for missing_node in storage_calc.missing_nodes.drain(..) {
-		if storage_calc.proof_store.has_proof(&missing_node) {
-		    let ok = storage_calc.trie.try_add_proof_from_proof_store(&missing_node, &storage_calc.proof_store).expect("should be able to insert proofs from proof store when they are found (storage trie)");
-		    assert!(ok, "proof is not added (storage trie)");
-		} else {
-		    storage_calc.missing_nodes_requested.push(missing_node.clone());
-		    fetcher.lock().add_missing_storage_node(&storage_calc.hashed_address, missing_node);
-		}
-	    }
-	});
+            let mut storage_calc = storage_calc.lock();
+            let storage_calc = &mut *storage_calc;
+            storage_calc.missing_nodes_requested.clear();
+            for missing_node in storage_calc.missing_nodes.drain(..) {
+                if storage_calc.proof_store.has_proof(&missing_node) {
+                    let ok = storage_calc.trie.try_add_proof_from_proof_store(&missing_node, &storage_calc.proof_store).expect("should be able to insert proofs from proof store when they are found (storage trie)");
+                    assert!(ok, "proof is not added (storage trie)");
+                } else {
+                    storage_calc.missing_nodes_requested.push(missing_node.clone());
+                    fetcher.lock().add_missing_storage_node(&storage_calc.hashed_address, missing_node);
+                }
+            }
+        });
 
         let mut fetcher = fetcher.lock();
         if !fetcher.is_empty() {
@@ -591,21 +607,21 @@ impl RootHashCalculator {
         }
 
         self.changed_account.read().par_iter().for_each(|(address, status)| {
-	    if !status.needs_processing() {
-		return;
-	    }
+            if !status.needs_processing() {
+                return;
+            }
             let storage_calc = self.get_account_storage(address);
-	    let mut storage_calc = storage_calc.lock();
-	    let storage_calc = &mut *storage_calc;
-	    for missing_node in storage_calc.missing_nodes_requested.drain(..) {
-		if storage_calc.proof_store.has_proof(&missing_node) {
-		    let ok = storage_calc.trie.try_add_proof_from_proof_store(&missing_node, &storage_calc.proof_store).expect("should be able to insert proofs from proof store when they are found (storage trie)");
-		    assert!(ok, "proof is not added (storage trie)");
-		} else {
-		    panic!("Missing node that was just fetched is not there (storage trie)");
-		}
-	    }
-	});
+            let mut storage_calc = storage_calc.lock();
+            let storage_calc = &mut *storage_calc;
+            for missing_node in storage_calc.missing_nodes_requested.drain(..) {
+                if storage_calc.proof_store.has_proof(&missing_node) {
+                    let ok = storage_calc.trie.try_add_proof_from_proof_store(&missing_node, &storage_calc.proof_store).expect("should be able to insert proofs from proof store when they are found (storage trie)");
+                    assert!(ok, "proof is not added (storage trie)");
+                } else {
+                    panic!("Missing node that was just fetched is not there (storage trie)");
+                }
+            }
+        });
         Ok(())
     }
 
@@ -857,7 +873,8 @@ impl RootHashCalculator {
         consistent_db_view: ConsistentDbView<Provider>,
         shared_cache: SharedCacheV2,
         outcome: &ExecutionOutcome,
-    ) -> Result<(B256, SparseTrieMetrics), SparseTrieError>
+        proof_targets: &HashSet<Address>,
+    ) -> Result<(B256, HashMap<Address, Vec<Bytes>>, SparseTrieMetrics), SparseTrieError>
     where
         Provider: DatabaseProviderFactory<Provider: BlockReader> + Send + Sync,
     {
@@ -867,7 +884,7 @@ impl RootHashCalculator {
         self.shared_cache = shared_cache.clone();
 
         stats.start();
-        self.prepare_changes_for_storage_trie(outcome)?;
+        self.prepare_changes_for_storage_trie(outcome, proof_targets)?;
         stats.measure_prepare(true);
         self.do_first_fetch(&consistent_db_view, &mut stats)?;
 
@@ -895,7 +912,7 @@ impl RootHashCalculator {
         stats.measure_prepare(false);
 
         let mut loop_break = false;
-        let mut result = B256::ZERO;
+        let mut root_hash = B256::ZERO;
         for _ in 0..10 {
             stats.start();
             let ok = self.process_account_tries_update()?;
@@ -907,17 +924,28 @@ impl RootHashCalculator {
                 continue;
             }
             stats.start();
-            result = self.hash_account_trie(&shared_cache);
+            root_hash = self.hash_account_trie(&shared_cache);
             stats.measure_hash(false);
             loop_break = true;
             break;
         }
         assert!(loop_break, "account trie are not processed after 10 iters");
 
+        let mut proofs = HashMap::default();
+        for proof_target in proof_targets {
+            let nibbles = Nibbles::unpack(keccak256(proof_target));
+            let proof = self
+                .account_trie
+                .trie
+                .proof(&nibbles, &shared_cache.account_trie)
+                .map_err(|err| SparseTrieError::Other(err.into()))?;
+            proofs.insert(*proof_target, proof);
+        }
+
         let mut metrics = SparseTrieMetrics::default();
         metrics.fetched_nodes = stats.fetched_nodes;
         stats.finalize_and_print();
-        Ok((result, metrics))
+        Ok((root_hash, proofs, metrics))
     }
 }
 

crates/rbuilder/Cargo.toml
@@ -53,6 +53,7 @@ alloy-rpc-types-eth.workspace = true
 alloy-consensus.workspace = true
 alloy-signer-local.workspace = true
 alloy-eips.workspace = true
+alloy-trie.workspace = true
 
 ethereum_ssz_derive.workspace = true
 ethereum_ssz.workspace = true

commit ffa5156b1a262f576bad1df981355343b246b34f

chore: revert block request removal from `BidObserver` (#710)
## 📝 Summary

Revert block request removal from `BidObserver`. Regression was
introduced in https://github.com/flashbots/rbuilder/pull/702

## ✅ I have completed the following steps:

* [x] Run `make lint`
* [x] Run `make test`
* [ ] Added tests (if applicable)

crates/rbuilder/src/live_builder/block_output/bid_observer.rs
@@ -25,6 +25,7 @@ impl BidObserver for NullBidObserver {
         &self,
         _slot_data: &MevBoostSlotData,
         _sealed_block: &SealedBlock,
+        _submit_block_request: &SubmitBlockRequest,
         _built_block_trace: &BuiltBlockTrace,
         _builder_name: String,
         _best_bid_value: U256,

crates/rbuilder/src/live_builder/block_output/bid_observer_multiplexer.rs
@@ -27,6 +27,7 @@ impl BidObserver for BidObserverMultiplexer {
         &self,
         slot_data: &MevBoostSlotData,
         sealed_block: &SealedBlock,
+        submit_block_request: &SubmitBlockRequest,
         built_block_trace: &BuiltBlockTrace,
         builder_name: String,
         best_bid_value: alloy_primitives::U256,

crates/rbuilder/src/live_builder/block_output/bidding_service_interface.rs
@@ -10,6 +10,7 @@ use tokio_util::sync::CancellationToken;
 use crate::{
     building::{builders::block_building_helper::BiddableUnfinishedBlock, BuiltBlockTrace},
     live_builder::payload_events::MevBoostSlotData,
+    mev_boost::submission::SubmitBlockRequest,
 };
 
 /// Trait that receives every bid made by us to the relays.
@@ -19,6 +20,7 @@ pub trait BidObserver: std::fmt::Debug {
         &self,
         slot_data: &MevBoostSlotData,
         sealed_block: &SealedBlock,
+        submit_block_request: &SubmitBlockRequest,
         built_block_trace: &BuiltBlockTrace,
         builder_name: String,
         best_bid_value: U256,
@@ -33,6 +35,7 @@ impl BidObserver for NullBidObserver {
         &self,
         _slot_data: &MevBoostSlotData,
         _sealed_block: &SealedBlock,
+        _submit_block_request: &SubmitBlockRequest,
         _built_block_trace: &BuiltBlockTrace,
         _builder_name: String,
         _best_bid_value: U256,

crates/rbuilder/src/live_builder/block_output/relay_submit.rs
@@ -298,7 +298,7 @@ async fn run_submit_to_relays_job(
             &cancel,
         );
 
-        if let Some((optimistic_signed_block, _)) = optimistic_signed_block {
+        let signed_block = if let Some((optimistic_signed_block, _)) = optimistic_signed_block {
             submit_block_to_relays(
                 &config.chain_spec,
                 &optimistic_signed_block,
@@ -311,6 +311,7 @@ async fn run_submit_to_relays_job(
                 &submission_span,
                 &cancel,
             );
+            optimistic_signed_block
         } else {
             // non-optimistic submission to optimistic relays
             submit_block_to_relays(
@@ -325,18 +326,27 @@ async fn run_submit_to_relays_job(
                 &submission_span,
                 &cancel,
             );
-        }
+            normal_signed_block
+        };
 
-        submission_span.in_scope(|| {
-            // NOTE: we only notify normal submission here because they have the same contents but different pubkeys
-            config.bid_observer.block_submitted(
-                &slot_data,
-                &block.sealed_block,
-                &block.trace,
-                builder_name,
-                bid_metadata.value.top_competitor_bid.unwrap_or_default(),
-            );
-        })
+        match signed_block.into_request(&config.chain_spec, None) {
+            Ok(request) => {
+                submission_span.in_scope(|| {
+                    // NOTE: we only notify normal submission here because they have the same contents but different pubkeys
+                    config.bid_observer.block_submitted(
+                        &slot_data,
+                        &block.sealed_block,
+                        &request,
+                        &block.trace,
+                        builder_name,
+                        bid_metadata.value.top_competitor_bid.unwrap_or_default(),
+                    );
+                })
+            }
+            Err(err) => {
+                error!(parent: &submission_span, err = ?err, "Error converting request for bid observer");
+            }
+        };
     }
 }
 