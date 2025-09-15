cf3a059ab88e06edac472bebb235d4bf28a7a2c4
Bharath Vedartham <vedabharath12345@gmail.com>
2025-09-15 11:41:28 +0530

    try sth

--- crates/rbuilder/src/live_builder/order_input/blob_type_order_filter.rs
+++ crates/rbuilder/src/live_builder/order_input/blob_type_order_filter.rs
@@ -30,12 +30,11 @@ pub fn new_pre_fusaka(
     sink: Box<dyn ReplaceableOrderSink>,
 ) -> BlobTypeOrderFilter<impl Fn(&TransactionSignedEcRecoveredWithBlobs) -> bool + Send + Sync> {
     BlobTypeOrderFilter::new(sink, |tx| {
-        // if tx.is_eip4844() {
-        //     matches!(*tx.blobs_sidecar, BlobTransactionSidecarVariant::Eip4844(_))
-        // } else {
-        //     true
-        // }
-        true
+        if tx.is_eip4844() {
+            matches!(*tx.blobs_sidecar, BlobTransactionSidecarVariant::Eip4844(_)) ||  matches!(*tx.blobs_sidecar, BlobTransactionSidecarVariant::Eip7594(_))
+        } else {
+            true
+        }
     })
 }
 
